52b5aaaec5d5b1c1431bc6615e17a345442a5b34
rapstar-surya <surajchaudhari357@gmail.com>
2023-05-11 12:12:35 +0530

    layout created usign grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Grid } from "@mui/material";
 
 export default function App() {
   return (
-    <div>
-      <h1>import all compononets here</h1>
-    </div>
-  )
+    <Grid container>
+      <Grid md={4}>
+
+      </Grid>
+      
+      <Grid md={4}>
+      <p>second column</p>
+      </Grid>
+
+      <Grid md={4}>
+        <p>third column</p>
+      </Grid>
+    </Grid>
+  );
 }
