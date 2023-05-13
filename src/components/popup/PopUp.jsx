97b3101296c1d76c5b716eabe0d5e059ed9845f2
rapstar-surya <surajchaudhari357@gmail.com>
2023-05-13 12:20:07 +0530

    remaining component added in popup folder

--- src/components/popup/PopUp.jsx
+++ src/components/popup/PopUp.jsx
@@ -1,32 +1,73 @@
 import React, { useState } from "react";
 import popup from "./PopUp.module.css";
-// import { useRecoilState } from "recoil";
-// import { toggleState } from "../../atom/Atom";
 
+import { Button } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
-
 import { useNavigate } from "react-router-dom";
+import InputBox from "./InputBox";
+import Comments from "./Comments";
 
 export default function PopUp() {
-  // const [toggle, setToggle] = useRecoilState(toggleState);
   const navigate = useNavigate();
+
+  // New state to track whether to show saved text or input box
+  const [showText, setShowText] = useState(false);
+
+  // New state to store the saved text
+  const [savedText, setSavedText] = useState("");
+
+  const handleSaveText = (text) => {
+    setSavedText(text);
+    setShowText(true);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault(); 
+      
+    }
+  };
 
   return (
     <>
       <div className={popup.mainDiv}>
         <div className={popup.title}>
-          <h2 className={popup.head}>📻 Cook Food</h2>
-          <span onClick={() => navigate('/')}>❌</span>
+          <h2 className={popup.head}>
+            <span>📻</span>{" "}
+            <span
+              contentEditable
+              suppressContentEditableWarning
+              onKeyDown={handleKeyDown}
+            >
+              {" "}
+              Cook Food{" "}
+            </span>
+          </h2>
+          <span onClick={() => navigate("/")}>❌</span>
         </div>
-        <span className={popup.para}>in list To Do</span>
+        <span className={popup.para}>
+          in list{" "}
+          <span
+            contentEditable
+            suppressContentEditableWarning
+            onKeyDown={handleKeyDown}
+          >
+            {" "}
+            To Do{" "}
+          </span>
+        </span>
         <div className={popup.des}>
           <MenuIcon sx={{ marginRight: "1rem" }} /> <h4>Description</h4>
         </div>
-        <input
-          className={popup.firstInputBox}
-          placeholder="Add a more detailed description..."
-        />
+        {!showText ? (
+          // Render input box if showText is false
+          <p className={popup.paraTwo} onClick={() => setShowText(true)}>
+            Add a more detailed description...
+          </p>
+        ) : (
+          <InputBox onSaveText={handleSaveText} /> // Pass onSaveText prop to InputBox component
+        )}
         <div className={popup.des}>
           <ReceiptLongIcon sx={{ marginRight: "1rem" }} /> <h4>Activity</h4>
         </div>
@@ -35,10 +76,9 @@ export default function PopUp() {
           className={popup.secondInputBox}
           placeholder="Write a comment..."
         />
+        <Button>Save</Button>
         <br /> <br />
-        <span className={popup.username}>PR</span>
-        <span className={popup.comments}>PR added this card to To Do</span>
-        <p className={popup.commentsTime}>a minute ago</p>
+        <Comments />
       </div>
     </>
   );
